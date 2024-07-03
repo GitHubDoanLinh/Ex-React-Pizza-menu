a61ad80d76bea1163e81bc163329fe1e3d9c3869
Doan Huu Linh <doanlinh1510@gmail.com>
2024-07-03 14:59:57 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
+        <div className={'container'}>
             <Header/>
             <Menu/>
             <Footer/>
@@ -57,7 +58,13 @@ function App() {
 }
 
 function Header() {
-    return <h1>Fast React Pizza </h1>
+    // const style = {color: "red", fontsize: "54px", textTransform: "uppercase"};
+    const style = {};
+    return(
+        <header className={'header'}>
+            <h1 style={style}>Fast React Pizza CO.</h1>
+        </header>
+    )
 }
 
 function Menu() {
