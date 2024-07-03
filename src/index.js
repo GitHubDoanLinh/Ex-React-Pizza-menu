b225e569ac4d0cf5399ca6360e1eb0bcb3df1bf9
Doan Huu Linh <doanlinh1510@gmail.com>
2024-07-03 16:18:39 +0700

    commit3 Passing and Receiving Props

--- src/index.js
+++ src/index.js
@@ -71,9 +71,28 @@ function Menu() {
     return (
         <div className={'menu'}>
             <h2>Our menu</h2>
-            <Pizza/>
-            <Pizza/>
-            <Pizza/>
+            <Pizza name='Pizza spinaci'
+                   ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+                   photoName='pizzas/spinaci.jpg'
+                   price={10}/>
+            <Pizza name='Pizza Funghi'
+                   ingredients='Tomato, mushrooms'
+                   photoName='pizzas/funghi.jpg'
+                   price={12}/>
+        </div>
+    )
+}
+
+function Pizza(props) {
+    console.log(props);
+    return (
+        <div className={'pizza'}>
+            <img src={props.photoName} alt={props.name}/>
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price + 1}</span>
+            </div>
         </div>
     )
 }
@@ -85,16 +104,6 @@ function Footer() {
     const isOpen = hour >= openHour && hour <= closeHour;
     console.log(isOpen);
     return <footer className={'footer'}>{new Date().toLocaleTimeString()}. We are currently open</footer>
-}
-
-function Pizza() {
-    return (
-        <div>
-            <img src={'pizzas/spinaci.jpg'} alt={'pizza spinaci'}/>
-            <h3>Pizza Spinaci</h3>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </div>
-    )
 }
 
 //React v18
